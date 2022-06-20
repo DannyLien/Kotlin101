75986bbf9d22daa39f6b887a39baacaa75cb108e
DannyLien <danny.lien1109@gmail.com>
2022-06-20 09:38:01 +0800

    606 enum class, 列舉資料帶參數

--- src/main/kotlin/poker/Card.kt
+++ src/main/kotlin/poker/Card.kt
@@ -2,8 +2,8 @@ package poker
 
 fun main() {
 
-    val c1 = Card(5, 2)
-    val c2 = Card(5, 1)
+    val c1 = Card(5, Face.CLUB)
+    val c2 = Card(5, Face.DIAMOND)
     println(c1)
     println(c2)
 
@@ -11,11 +11,15 @@ fun main() {
 
 }
 
-data class Card(val point: Int, val face: Int) : Comparable<Card> {
+enum class Face(val value: Int) {
+    CLUB(1), DIAMOND(2), HEART(3), SPADE(4)
+}
+
+data class Card(val point: Int, val face: Face) : Comparable<Card> {
     override fun compareTo(other: Card): Int {
         val diff = point - other.point
         return when (diff) {
-            0 -> face - other.face
+            0 -> face.value - other.face.value
             else -> diff
         }
     }
